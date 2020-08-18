5e28352be7d065610fa6c5c0607957811e4e0c5b
wyntonchin <goodcar@163.com>
2020-08-18 21:10:24 +0800

    网址参数

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:provide/provide.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter_easyrefresh/easy_refresh.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -46,7 +48,8 @@ class _HomePageState extends State<HomePage>
               print(data);
               List<Map> swiperDataList =
                   (data['data']['slides'] as List).cast();
-              List<Map> category = (data['data']['category'] as List).cast();
+              List<Map> navigatorList =
+                  (data['data']['category'] as List).cast();
               List<Map> recommendList =
                   (data['data']['recommend'] as List).cast();
               List<Map> floor1 = (data['data']['floor1'] as List).cast();
@@ -64,11 +67,21 @@ class _HomePageState extends State<HomePage>
                   loadReadyText: KString.loadReadyText,
                 ),
                 child: ListView(
-                  children: <Widget>[],
+                  children: <Widget>[
+                    //轮播
+                    SwiperDiy(
+                      swiperDataList: swiperDataList,
+                    ),
+                    //分类
+                    TopNavigator(
+                      navigatorList: navigatorList,
+                    ),
+                  ],
                 ),
                 loadMore: () async {
                   print('开始加载更多');
                 },
+                // refreshHeader: ClassicsHeader(),
               );
             } else {
               return Container(
@@ -77,5 +90,82 @@ class _HomePageState extends State<HomePage>
             }
           },
         ));
+  }
+}
+
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  const SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      height: 122,
+      width: 750,
+      child: Swiper(
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {},
+            child: Image.network(
+              "${swiperDataList[index]['image']}",
+              fit: BoxFit.cover,
+            ),
+          );
+        },
+        itemCount: swiperDataList.length,
+        pagination: SwiperPagination(),
+        autoplay: true,
+        autoplayDelay: 3000,
+      ),
+    );
+  }
+}
+
+//上部分类组件
+class TopNavigator extends StatelessWidget {
+  final List navigatorList;
+  const TopNavigator({Key key, this.navigatorList}) : super(key: key);
+
+  Widget _gridViewItemUI(BuildContext context, item, index) {
+    return InkWell(
+      onTap: () {
+        //跳转到分类页面
+      },
+      child: Column(
+        children: <Widget>[
+          Image.network(
+            item['image'],
+            width: 55,
+            height: 50,
+          ),
+          Text(item['firstCategoryName']),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (navigatorList.length > 10) {
+      navigatorList.removeRange(10, navigatorList.length);
+    }
+    var tempIndex = -1;
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.only(top: 5),
+      padding: EdgeInsets.all(3),
+      height: 200,
+      child: GridView.count(
+        crossAxisCount: 5,
+        //禁止滚动
+        physics: NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.all(3),
+        children: navigatorList.map((item) {
+          tempIndex++;
+          return _gridViewItemUI(context, item, tempIndex);
+        }).toList(),
+      ),
+    );
   }
 }
